d784a735e2890eb80f44808f893e57cc94b0aed4
kymec <kummaz@ukr.net>
2019-04-21 17:30:32 +0300

    ready

---
+++ script.js
@@ -0,0 +1,33 @@
+"use strict"
+//1) Задание
+var arr = [2, -5, 1, -2, 0, -3, 2, -14]
+var posArr = arr.filter(function(number){
+    return number >= 0;
+});
+var sqrtArr = posArr.map(function(value){
+    return  Math.sqrt(value);
+});
+console.log(sqrtArr);
+
+//2) Задание
+var arr2 = [4, 3, 5, 5, 0, 4, 8, 6];
+var result = 0; // сумма чисел
+var isChange=false; // проверка на случай если в массиве больше одного ноля
+arr2.reduce(function(sum, current){
+    if (current == 0 && !isChange){
+        result = sum;
+        isChange = true;
+    }
+    return sum + current
+});
+console.log(result);
+
+//3) Задание
+function summArguments(){
+    var sum = 0;
+    for(var i = 0; i < arguments.length; i++){
+        sum += arguments[i];
+    }
+    return sum;
+}
+console.log(summArguments(10, 1, 2, 5));
